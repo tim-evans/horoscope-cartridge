e2afa602a14a567ab25a478ef82ffdae70b62fd1
Tim Evans <tevans@movableink.com>
2015-09-25 15:11:16 -0400

    get js injected into the html

--- Brocfile.js
+++ Brocfile.js
@@ -1,13 +1,33 @@
 /* global require, module, process */
 function Cartridge() {
+  var Plugin = require('broccoli-plugin');
   var Funnel = require('broccoli-funnel');
   var concat = require('broccoli-concat');
   var mergeTrees = require('broccoli-merge-trees');
   var esTranspiler = require('broccoli-babel-transpiler');
   var env = process.env.ENV;
   var fs = require('fs');
+  var path = require('path');
   var yaml = require('yaml-js');
   var manifest = yaml.load(fs.readFileSync('./cartridge.yml'));
+
+  EmbedAssets.prototype = Object.create(Plugin.prototype);
+  EmbedAssets.prototype.constructor = EmbedAssets;
+  function EmbedAssets(inputNodes, options) {
+    if (!(this instanceof EmbedAssets)) { return new EmbedAssets(inputNodes, options); }
+    Plugin.call(this, [inputNodes]);
+
+    this.outputFile = options.outputFile;
+  }
+  EmbedAssets.prototype.build = function () {
+    var tree = this.inputPaths[0];
+    var script = fs.readFileSync(tree + '/main.js');
+    var index = fs.readFileSync('index.html').toString();
+
+    fs.writeFileSync(path.join(this.outputPath, this.outputFile),
+                     index.replace('{{script}}', script)
+                          .replace(/\{\{name\}\}/g, manifest.name));
+  }
 
   var transpile = function (tree, opts) {
     return esTranspiler(tree, {
@@ -64,15 +84,18 @@ function Cartridge() {
     ],
     outputFile: '/vendor.js'
   });
+
   var uglify = require('broccoli-uglify-js');
   vendor = uglify(vendor);
 
-  return concat(mergeTrees([vendor, js]), {
+  return new EmbedAssets(concat(mergeTrees([vendor, js]), {
     inputFiles: [
       'vendor.js',
       'main.amd.js'
     ],
-    outputFile: '/' + manifest.name + '.js'
+    outputFile: '/main.js'
+  }), {
+    outputFile: '/index.html'
   });
 }
 
