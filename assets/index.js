33c3a8f61ee76ab26bf434d351eee4e9317e3a26
Tim Evans <tevans@movableink.com>
2015-10-06 17:02:11 -0400

    add templates file

--- assets/index.js
+++ assets/index.js
@@ -1,17 +1,48 @@
-import getAstrologySign from 'horoscope/astrology-sign';
+import getAstrologySign from 'horoscope/lib/astrology-sign';
 
 const BASE_URL = 'http://www.huffingtonpost.com/horoscopes/astrology/';
+
+function $(selector) {
+  if (selector.split(' ').length === 1 && selector.indexOf('#') === 0) {
+    return document.getElementById(selector.slice(1));
+  }
+
+  let nodes = Array.prototype.slice.call(document.querySelectorAll(selector));
+
+  return nodes.map(function (node) {
+    node.$ = node.querySelectorAll;
+    return node;
+  });
+}
+
+function HTML(text) {
+  let doc = document.implementation.createHTMLDocument('horoscope');
+  doc.documentElement.innerHTML = text;
+
+  this.document = doc;
+}
+
+HTML.prototype.$ = function (selector) {
+  if (selector.split(' ').length === 0 && selector.indexOf('#') === 0) {
+    return this.document.getElementById(selector.slice(1));
+  }
+
+  let nodes = Array.prototype.slice.call(this.document.querySelectorAll(selector));
+
+  return nodes.map(function (node) {
+    node.$ = node.querySelectorAll;
+    return node;
+  });
+}
 
 export default function() {
   let sign = getAstrologySign(CD.param('birthday'));
 
   CD.getCORS(BASE_URL + sign, function (text) {
-    let doc = document.implementation.createHTMLDocument('horoscope');
-    doc.documentElement.innerHTML = text;
+    let html = new HTML(text);
+    let todaysHoroscope = html.$('#dailyScope .horRiverBody')[1];
+    let horoscope = todaysHoroscope.$('p')[0].innerHTML;
 
-    let todaysHoroscope = CD.$('#dailyScope .horRiverBody', doc)[1];
-    let horoscope = todaysHoroscope.querySelectorAll('p')[0].innerHTML;
-
-    document.getElementById('mi_size_container').innerHTML = `<h1>${sign}</h1><p>${horoscope}</p>`;
+    $('#mi_size_container').innerHTML = `<h1>${sign}</h1><p>${horoscope}</p>`;
   });
 }
